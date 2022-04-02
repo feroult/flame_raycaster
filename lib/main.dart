1c4fddc2f5e2f7a6ff4fbfacb399a385941d5fcd
Fernando Ultremare <feroult@gmail.com>
2022-04-01 21:36:18 -0300

    v0

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,18 @@
-import 'dart:typed_data';
 import 'dart:ui';
-import 'dart:ui' as ui;
 
 import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flame_raycaster/joystick.dart';
-import 'package:flame_raycaster/level.dart';
 import 'package:flame_raycaster/utils.dart';
 import 'package:flame_raycaster/xgame.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-
-import 'buttons.dart';
 
 void main() async {
   await WidgetsFlutterBinding.ensureInitialized();
   await Flame.device.setLandscape();
   await Flame.device.setLandscapeLeftOnly();
   final game = RaycasterExempleGame();
-  // final game = RaycasterGame();
   runApp(GameWidget(game: game));
 }
 
@@ -28,67 +21,6 @@ class RaycasterExempleGame extends FlameGame with HasDraggables, HasTappables {
   Future<void> onLoad() async {
     add(new RaycasterComponent());
     await JoystickBuilder.build(this);
-  }
-}
-
-class RaycasterGame extends FlameGame {
-  final viewSize = Size(640, 360);
-  late Rect bounds;
-
-  final deviceTransform = Float64List(16);
-
-  late Offset offset;
-
-  late Level level;
-  late XGame game;
-
-  RaycasterGame() {
-    bounds = Offset.zero & viewSize;
-  }
-
-  handleMetricsChanged() async {
-    final size = window.physicalSize;
-    final pixelRatio = size.shortestSide / viewSize.shortestSide;
-
-    deviceTransform
-      ..[0] = pixelRatio
-      ..[5] = pixelRatio
-      ..[10] = 1
-      ..[15] = 1;
-
-    offset = (size / pixelRatio - viewSize as Offset) * 0.5;
-  }
-
-  @override
-  Future<void> onLoad() async {
-    level = await loadLevel('data/level2.json');
-    game = XGame(this.viewSize, level);
-    await handleMetricsChanged();
-  }
-
-  @override
-  void onGameResize(Vector2 size) {
-    super.onGameResize(size);
-    handleMetricsChanged();
-  }
-
-  @override
-  void update(double dt) {
-    super.update(dt);
-    game.update(dt, (int i) => false);
-  }
-
-  @override
-  void render(Canvas canvas) {
-    super.render(canvas);
-
-    canvas.save();
-    // canvas.translate(offset.dx, offset.dy);
-    // canvas.clipRect(bounds);
-    game.render(canvas);
-    canvas.restore();
-
-    // canvas.transform(deviceTransform);
   }
 }
 
